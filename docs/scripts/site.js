935f77af0b1cca0ddf2b959be354c55ae43f54b1
mayaAtanasova <mayaatanasova@gmail.com>
2021-12-10 18:13:14 +0200

    End-of-day

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -1,3 +1,26 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector('body').classList.add('loaded');
+    afterLoading();
 });
+
+
+function afterLoading(){
+    const gallery = document.querySelector('.gallery');
+    const modal = document.querySelector('.modal');
+
+    gallery.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (event.target.tagName == 'SPAN'){
+            const currSpan = event.target;
+            const img = event.target.children[0];
+            modal.style.display = 'block';
+            modal.querySelector('span').appendChild(img);
+            modal.querySelector('button').addEventListener('click', event => {
+                modal.querySelector('span').removeChild(img);
+                currSpan.appendChild(img);
+                modal.style.display = 'none';
+            });
+        
+        }
+    });
+}
